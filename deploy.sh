1cec19eebedbb9a15840600516ca472131449c4b
Jonas Merkle [JJM] <jonas_merkle@ymail.com>
2018-12-03 13:26:31 +0100

    ci

--- deploy.sh
+++ deploy.sh
@@ -18,6 +18,22 @@ eval "$(ssh-agent -s)"
 
 # If some kind of formatting is necessary do it here
 
+# Get the current gh-pages branch
+mkdir deployPDF
+cd deployPDF
+git clone -b gh-pages https://github.com/SoPra-Team-10/SopraGruppe10.git
+cd SopraGruppe10
+git config --global push.default simple
+git config user.name "Travis CI"
+git config user.email "travis@travis-ci.org"
+rm -rf *
+cp -R ../../output output
+git add --all
+git commit -m "Deploy code docs to GitHub Pages Travis build: ${TRAVIS_BUILD_NUMBER}" -m "Commit: ${TRAVIS_COMMIT}"
+git push --force "https://${GH_REPO_TOKEN}@github.com/SoPra-Team-10/SopraGruppe10.git" > /dev/null
+cd ../..
+rm -rf /deployPDF
+
 figlet GIT FORWARD
 
 case "$commitAuthorName" in
@@ -44,19 +60,19 @@ esac
 figlet SSH GIT
 
 # Initialize ssh
-echo "$sshKey" > ~/.ssh/key
-chmod 600 ~/.ssh/key
-ssh-add ~/.ssh/key
+#echo "$sshKey" > ~/.ssh/key
+#chmod 600 ~/.ssh/key
+#ssh-add ~/.ssh/key
 
 figlet GIT
 
 # Push the Repo
-git config --global user.email "$commitAuthorEMail"
-git config --global user.name "$commitAuthorName"
+#git config --global user.email "$commitAuthorEMail"
+#git config --global user.name "$commitAuthorName"
 
-git remote add gitlab git@github.com:aul12/Test.git
-git status
-git checkout -f master
+#git remote add gitlab git@github.com:aul12/Test.git
+#git status
+#git checkout -f master
 #git push gitlab master
 
 figlet FINISHED
