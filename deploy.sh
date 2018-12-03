58b9bdb925b084c30691da457eece66449e25145
Paul Nykiel <paul.nykiel@gmail.com>
2018-12-03 17:27:11 +0100

    Readded git forwarding

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 figlet STARTING DEPLOY
 commitHash="$1"
+commitMessage=$(git --no-pager log -1 --pretty="%B")
 commitAuthorEMail=$(git --no-pager log -1 --pretty="%cE")
 commitAuthorName=$(git --no-pager log -1 --pretty="%aN")
 echo "Last commit: $commitHash by $commitAuthorName with email $commitAuthorEMail"
@@ -29,4 +30,14 @@ cd ../..
 
 # Remove all output, we don't want to commit binaries
 rm -rf output/
-# TODO clone the complete repo, add the current files as a subdirectory and create a commit
+git clone "https://${GH_REPO_TOKEN}@github.com/SoPra-Team-10/Complete.git"
+cd Complete/
+git config --global push.default simple
+git config user.name "$commitAuthorName"
+git config user.email "$commitAuthorEMail"
+mkdir -p Uebungsblaetter
+cp ../README.md Uebungsblaetter/
+cp -r ../Meilenstein* Uebungsblaetter/
+git add -A
+git commit -m "$commitMessage"
+git push -f  "https://${GH_REPO_TOKEN}@github.com/SoPra-Team-10/Complete.git" master
