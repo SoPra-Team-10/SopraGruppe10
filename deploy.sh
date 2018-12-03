804c543275d88707ea063d14ecd9438eb1ffcd20
Paul Nykiel <paul.nykiel@gmail.com>
2018-12-03 16:52:52 +0100

    Removed git forwarding

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,6 @@ commitAuthorEMail=$(git --no-pager log -1 --pretty="%cE")
 commitAuthorName=$(git --no-pager log -1 --pretty="%aN")
 echo "Last commit: $commitHash by $commitAuthorName with email $commitAuthorEMail"
 
-# If some kind of formatting is necessary do it here
-
 # Get the current gh-pages branch
 mkdir deployPDF
 cd deployPDF
@@ -27,50 +25,8 @@ git commit -m "Deploy code docs to GitHub Pages Travis build: ${TRAVIS_BUILD_NUM
 git push force "https://${GH_REPO_TOKEN}@github.com/SoPra-Team-10/Uebungsblaetter.git" > /dev/null
 cd ../..
 rm -rf /deployPDF
+cd ../..
 
-figlet GIT FORWARD
-
-case "$commitAuthorName" in
-    "Paul Nykiel")
-        sshKey="$GITLAB_TOKEN_0"
-        ;;
-    "jonas-merkle")
-        sshKey="$GITLAB_TOKEN_1"
-        ;;
-    "Timmifixedit")
-        sshKey="$GITLAB_TOKEN_2"
-        ;;
-    "TarikEnderes")
-        sshKey="$GITLAB_TOKEN_3"
-        ;;
-    "4")
-        sshKey="$GITLAB_TOKEN_4"
-        ;;
-    "5")
-        sshKey="$GITLAB_TOKEN_5"
-        ;;
-esac
-sshKey="$GITLAB_TOKEN_0" # TODO remove me when everything works
-
-figlet SSH GIT
-mkdir -p ~/.ssh
-printf "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
-eval "$(ssh-agent -s)"
-
-# Initialize ssh
-echo "$sshKey" > ~/.ssh/key
-chmod 600 ~/.ssh/key
-ssh-add ~/.ssh/key
-
-figlet GIT
-
-# Push the Repo
-git config --global user.email "$commitAuthorEMail"
-git config --global user.name "$commitAuthorName"
-
-git remote add gitlab git@github.com:aul12/Test.git
-git status
-git checkout -f master
-#git push -f gitlab master
-
-figlet FINISHED
+# Remove all output, we don't want to commit binaries
+rm -rf output/
+# TODO clone the complete repo, add the current files as a subdirectory and create a commit
