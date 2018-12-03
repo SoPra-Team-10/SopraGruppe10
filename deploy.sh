655dc5cf63e50593529b8fa9a7cc3d0f5946e70c
Paul Nykiel <paul.nykiel@gmail.com>
2018-12-03 13:49:31 +0100

    Updated the ci, added dependencies to the README

--- deploy.sh
+++ deploy.sh
@@ -10,12 +10,6 @@ mkdir -p ~/.ssh
 printf "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
 eval "$(ssh-agent -s)"
 
-#figlet DEPLOY PDFS
-#echo "$DEPLOY_KEY" | xxd -r -p > ~/.ssh/deployKey
-#chmod 600 ~/.ssh/deployKey
-#ssh-add ~/.ssh/deployKey
-#scp -i ~/.ssh/deployKey output/*.pdf paul@aul12.me:/var/www/html/Sopra/
-
 # If some kind of formatting is necessary do it here
 
 # Get the current gh-pages branch
@@ -30,7 +24,7 @@ rm -rf *
 cp -R ../../output/* .
 git add --all
 git commit -m "Deploy code docs to GitHub Pages Travis build: ${TRAVIS_BUILD_NUMBER}" -m "Commit: ${TRAVIS_COMMIT}"
-git push origin gh-pages --force "https://${GH_REPO_TOKEN}@github.com/SoPra-Team-10/SopraGruppe10.git" > /dev/null
+git push origin gh-pages --force "https://${GH_REPO_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" origin gh-pages > /dev/null
 cd ../..
 rm -rf /deployPDF
 
@@ -60,19 +54,19 @@ esac
 figlet SSH GIT
 
 # Initialize ssh
-#echo "$sshKey" > ~/.ssh/key
-#chmod 600 ~/.ssh/key
-#ssh-add ~/.ssh/key
+echo "$sshKey" > ~/.ssh/key
+chmod 600 ~/.ssh/key
+ssh-add ~/.ssh/key
 
 figlet GIT
 
 # Push the Repo
-#git config --global user.email "$commitAuthorEMail"
-#git config --global user.name "$commitAuthorName"
+git config --global user.email "$commitAuthorEMail"
+git config --global user.name "$commitAuthorName"
 
-#git remote add gitlab git@github.com:aul12/Test.git
-#git status
-#git checkout -f master
-#git push gitlab master
+git remote add gitlab git@github.com:aul12/Test.git
+git status
+git checkout -f master
+#git push -f gitlab master
 
 figlet FINISHED
