736dcf9c955cadff8103f2a26de7c22d64908694
Paul Nykiel <paul.nykiel@gmail.com>
2018-12-04 17:24:12 +0100

    Hopefully fixed the link

--- pullRequestComment.sh
+++ pullRequestComment.sh
@@ -2,6 +2,6 @@
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]
 then
    curl -H "Authorization: token ${GH_REPO_TOKEN}" -X POST \
-    -d "{\"body\": \"View the pdf on: https://sopra-team-10.github.io/Uebungsblaetter/$TRAVIS_BRANCH/\"}" \
+    -d "{\"body\": \"View the pdf on: https://sopra-team-10.github.io/Uebungsblaetter/$TRAVIS_PULL_REQUEST_BRANCH/\"}" \
     "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
 fi
